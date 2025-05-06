9155fde99bc355daa73b5f6cfeb0e644b3fbbc6d
Francisco Barbosa <f_barbosa@outlook.pt>
2025-05-06 11:47:24 +0100

    added dataset and added vs code config files to gitignore

---
+++ Model.py
@@ -0,0 +1,7 @@
+#import kagglehub
+
+# Download latest version
+#path = kagglehub.dataset_download("uom190346a/disease-symptoms-and-patient-profile-dataset")
+
+#print("Path to dataset files:", path)
+
