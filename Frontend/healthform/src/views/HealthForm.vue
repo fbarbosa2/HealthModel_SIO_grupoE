26846a520c5588f0f7beab40a5efa7b5f77a6d92
paul0coelho <paulosmcoelho04@gmail.com>
2025-05-20 10:25:37 +0100

    Update HealthForm.vue

--- Frontend/healthform/src/views/HealthForm.vue
+++ Frontend/healthform/src/views/HealthForm.vue
@@ -171,6 +171,8 @@ export default {
     color: #fff;
     font-family: 'Inter', sans-serif;
     box-shadow: 0 8px 32px rgba(31, 38, 135, 0.37);
+    margin-top: 30px;
+    margin-bottom: 30px;
 }
 
 h1 {
